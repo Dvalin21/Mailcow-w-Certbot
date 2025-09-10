246989a7587ddcb7ccf9f0c761bcaee9d0ec9c79
Dvalin21 <dvalin21@gmail.com>
2025-09-10 01:06:29 -0500

    /data/conf/certbot/deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Reload nginx configuration after certificate renewal
+# Reload nginx configuration after certificate renewal /data/conf/certbot/deploy.sh
 docker exec mailcowdockerized-nginx-mailcow-1 nginx -s reload
